98e080f9017f093f85ff96da7b484a344210397a
rajeevbh <rajeevbh15290@gmail.com>
2023-08-23 09:54:13 -0400

    Updating Day5

---
+++ Day5/PwdGenerator.py
@@ -0,0 +1,6 @@
+# Welcome to the password Generator
+
+num_letters = int(input("How many letters would you like in your password?\n"))
+num_symbols = int(input("How many symbols would you like in your password?\n"))
+num_numbers = int(input("How many numbers would you like in your password?\n"))
+
